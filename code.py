f8c63fd03d6bcd6cbb3643b011d9076d557c7b2a
Sachin Singh <sachinishu02@gmail.com>
2023-08-03 09:48:19 +0530

    Update code.py

--- code.py
+++ code.py
@@ -15,5 +15,19 @@ training_set= pd.read_csv('ml-100k/u1.base', delimiter='\t')
 training_set= np.array(training_set, dtype='int')
 test_set= pd.read_csv('ml-100k/u1.test', delimiter='\t')
 test_set= np.array(test_set, dtype='int')
+
+#getting the no. of users and movies
 nb_users= int(max(max(training_set[:,0]),max(test_set[:,0])))
 nb_movies= int(max(max(training_set[:,1]),max(test_set[:,1])))
+
+#converting the data into an array with users in lines and ratings in columns
+def convert(data):
+    new_data= []
+    for id_users in range(1,nb_users+1):
+        id_movies= data[:,1][data[:,0]== id_users]
+        id_users= data[:,2][data[:,0]== id_users]
+        ratings= np.zeros(nb_movies)
+        new_data.append(list(ratings))
+    return new_data
+training_set= convert(training_set)
+test_set= convert(test_set)
