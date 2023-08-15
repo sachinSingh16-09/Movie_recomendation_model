9fd6d0bab1399d3e707342c098bab8f37708667d
Sachin Singh <sachinishu02@gmail.com>
2023-08-16 00:53:49 +0530

    Update autoencoders.py

--- autoencoders.py
+++ autoencoders.py
@@ -15,3 +15,19 @@ training_set= pd.read_csv('ml-100k/u1.base', delimiter='\t')
 training_set= np.array(training_set, dtype='int')
 test_set= pd.read_csv('ml-100k/u1.test', delimiter='\t')
 test_set= np.array(test_set, dtype='int')
+nb_users= int(max(max(training_set[:,0]),max(test_set[:,0])))
+nb_movies= int(max(max(training_set[:,1]),max(test_set[:,1])))
+
+#converting the data into an array with users in lines and ratings in columns
+
+def convert(data):
+    new_data= []
+    for id_users in range(1,nb_users+1):
+        id_movies= data[:,1][data[:,0]== id_users]
+        id_ratings= data[:,2][data[:,0]== id_users]
+        ratings= np.zeros(nb_movies)
+        ratings[id_movies-1]= id_ratings
+        new_data.append(list(ratings))
+    return new_data
+training_set= convert(training_set)
+test_set= convert(test_set)
