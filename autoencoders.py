51c3833292100afaa939c95039aaf6ee75e44f4f
Sachin Singh <sachinishu02@gmail.com>
2023-08-18 23:34:59 +0530

    Update autoencoders.py

--- autoencoders.py
+++ autoencoders.py
@@ -75,3 +75,19 @@ for epoch in range(1, nb_epoch + 1):
             s += 1.
             optimizer.step()
     print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
+
+# testing the SAE
+    test_loss=0
+    s = 0.
+    for id_user in range(nb_users):
+        input = Variable(training_set[id_user]).unsqueeze(0)
+        target = Variable(test_set[id_user]).unsqueeze(0)
+        if torch.sum(target.data>0)>0:
+            output = sae(input)
+            target.require_grad= False
+            output[target ==0]=0 
+            loss = criterion(output, target)
+            mean_corrector = nb_movies/float(torch.sum(target.data>0)+ 1e-10)
+            test_loss += np.sqrt(loss.item()*mean_corrector)
+            s += 1.
+    print('loss: '+str(test_loss/s))
